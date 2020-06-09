3f7588653e2f7e76e2a6919b2b4f47bcb211ddb2
edigurmani <32576943+edigurmani@users.noreply.github.com>
2020-06-09 20:55:12 +0200

    added edi gurmani texts

---
+++ en.php
@@ -0,0 +1,115 @@
+<?php
+
+
+/*
+------------------
+Language: English
+------------------
+*/
+ 
+$lang = array();
+ 
+//*****************************************
+//NAVBAR HEADER
+//*****************************************
+
+$lang["navbar_index_button_text"] = 'Home';
+$lang["navbar_octave_commands_button_text"] = 'Octave commands';
+$lang["navbar_api_description_button_text"] = 'API description';
+$lang["navbar_individual_tasks_button_text"] = 'Individual tasks';
+$lang["navbar_individual_tasks_submenu_1_button_text"] = 'Szabóová Klaudia';
+$lang["navbar_individual_tasks_submenu_2_button_text"] = 'Edi Gurmani';
+$lang["navbar_individual_tasks_submenu_3_button_text"] = 'Dat NguyenThe';
+$lang["navbar_individual_tasks_submenu_4_button_text"] = 'Blahó András';
+$lang["navbar_statistics_button_text"] = 'Statistics';
+
+//*****************************************
+//NAVBAR FOOTER
+//*****************************************
+
+$lang["footer_main_h6_1"] = 'About';
+$lang["footer_about_text"] = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nisi risus, aliquet a tortor ac, convallis eleifend lacus. Nam sed hendrerit velit. Sed in tortor cursus, posuere arcu vel, sodales velit. Nam elit dui, pharetra at massa sit amet, ultricies finibus nunc. Cras elementum vestibulum dapibus. - EN';
+
+$lang["footer_main_h6_2"] = 'Menu';
+$lang["footer_index_button_text"] = 'Home';
+$lang["footer_octave_commands_button_text"] = 'Octave commands';
+$lang["footer_api_description_button_text"] = 'API description';
+$lang["footer_statistics_button_text"] = 'Statistics';
+
+$lang["footer_main_h6_3"] = 'Individual tasks';;
+$lang["footer_individual_tasks_1_button_text"] = 'Szabóová Klaudia';
+$lang["footer_individual_tasks_2_button_text"] = 'Edi Gurmani';
+$lang["footer_individual_tasks_3_button_text"] = 'Dat NguyenThe';
+$lang["footer_individual_tasks_4_button_text"] = 'Blahó András';
+
+$lang["footer_copyright_text"] = 'Web technologies 2 - FEI STU - 2020 summer semester';
+
+//*****************************************
+//INDEX.PHP
+//*****************************************
+
+$lang["index_main_h1_text"] = 'Home';
+$lang["index_main_h2_text"] = 'Lorem ipsum - EN';
+$lang["index_main_p_text"] = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nisi risus, aliquet a tortor ac, convallis eleifend lacus. Nam sed hendrerit velit. Sed in tortor cursus, posuere arcu vel, sodales velit. Nam elit dui, pharetra at massa sit amet, ultricies finibus nunc. Cras elementum vestibulum dapibus. Nullam pharetra erat non aliquam suscipit. Nulla semper mauris at dolor pulvinar, non vulputate arcu dictum. Aliquam dapibus gravida suscipit. Duis ipsum lectus, sodales sit amet feugiat sit amet, interdum quis leo. Nulla luctus velit at blandit congue. Donec tristique, elit a semper porttitor, purus erat tincidunt metus, tincidunt molestie ex nulla in purus.';
+
+//*****************************************
+//APIDESCRIPTION.PHP
+//*****************************************
+
+$lang["apidescription_main_h1_text"] = 'API description';
+
+//*****************************************
+//OCTAVECOMMANDS.PHP
+//*****************************************
+
+$lang["octavecommands_main_h1_text"] = 'Octave commands';
+$lang["octavecommands_input_text"] = 'Write a statement';
+$lang["octavecommands_btn_text"] = 'Calculate!';
+
+//*****************************************
+//STATISTICS.PHP
+//*****************************************
+
+$lang["statistics_main_h1_text"] = 'Statistics';
+$lang["statistics_table_header_1"] = 'Individual task name';
+$lang["statistics_table_header_2"] = 'Used (times)';
+$lang["statistics_button"] = 'Send statistics to email';
+
+//-----------------------------------------
+// !!!!! INDIVIDUAL TASKS START !!!!!
+//-----------------------------------------
+
+//Szabóová Klaudia
+$lang["szaboovaklaudia_main_h1_text"] = 'Szabóová Klaudia - car suspension ';
+$lang["szaboovaklaudia_input"] = 'Height of the barrier';
+$lang["szaboovaklaudia_button"] = 'Calculate';
+$lang["szaboovaklaudia_check_1"] = 'Graph';
+$lang["szaboovaklaudia_check_2"] = 'Animation';
+$lang["szaboovaklaudia_error"] = 'The input range is from -0.3 to 0.3!';
+$lang["szaboovaklaudia_graf_label_1"] = 'Movement of the wheel';
+$lang["szaboovaklaudia_graf_label_2"] = 'Movement of the car';
+
+//Edi Gurmani
+$lang["edigurmani_main_h1_text"] = 'Edi Gurmani - Ball and Beam';
+$lang["edigurmani_check_1"] = 'View graph';
+$lang["edigurmani_check_2"] = 'View animation';
+$lang["edigurmani_error"] = 'The input range is from 0 to 0.8!';
+$lang["edigurmani_button"] = 'Calculate';
+
+
+
+//Dat NguyenThe
+$lang["datnguyenthe_main_h1_text"] = 'Dat NguyenThe - name of the task';
+$lang["datnguyenthe_example_1"] = 'Example 1';
+$lang["datnguyenthe_example_2"] = 'Example 2';
+
+//Blahó András
+$lang["blahoandras_main_h1_text"] = 'Blahó András - airplane';
+$lang["blahoandras_input_value_label"] = 'New pitch of the aircraft';
+$lang["blahoandras_button_label"] = 'Go!';
+$lang["blahoandras_checkbox_graf_label"] = 'View graph';
+$lang["blahoandras_checkbox_animation_label"] = 'View animation';
+$lang["blahoandras_graf_1_label"] = 'Pitch of the aircraft';
+$lang["blahoandras_graf_2_label"] = 'Pitch of the elevator';
+
+?>
---
+++ sk.php
@@ -0,0 +1,115 @@
+<?php
+
+
+/*
+------------------
+Language: Slovak
+------------------
+*/
+ 
+$lang = array();
+ 
+//*****************************************
+//NAVBAR HEADER
+//*****************************************
+ 
+$lang["navbar_index_button_text"] = 'Hlavná stránka';
+$lang["navbar_api_description_button_text"] = 'Popis API';
+$lang["navbar_octave_commands_button_text"] = 'Octave príkazy';
+$lang["navbar_individual_tasks_button_text"] = 'Individuálne úlohy';
+$lang["navbar_individual_tasks_submenu_1_button_text"] = 'Szabóová Klaudia';
+$lang["navbar_individual_tasks_submenu_2_button_text"] = 'Edi Gurmani';
+$lang["navbar_individual_tasks_submenu_3_button_text"] = 'Dat NguyenThe';
+$lang["navbar_individual_tasks_submenu_4_button_text"] = 'Blahó András';
+$lang["navbar_statistics_button_text"] = 'Štatistika';
+
+//*****************************************
+//NAVBAR FOOTER
+//*****************************************
+
+$lang["footer_main_h6_1"] = 'O stránke';
+$lang["footer_about_text"] = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nisi risus, aliquet a tortor ac, convallis eleifend lacus. Nam sed hendrerit velit. Sed in tortor cursus, posuere arcu vel, sodales velit. Nam elit dui, pharetra at massa sit amet, ultricies finibus nunc. Cras elementum vestibulum dapibus. - SK';
+
+$lang["footer_main_h6_2"] = 'Menu';
+$lang["footer_index_button_text"] = 'Hlavná stránka';
+$lang["footer_api_description_button_text"] = 'Popis API';
+$lang["footer_octave_commands_button_text"] = 'Octave príkazy';
+$lang["footer_statistics_button_text"] = 'Štatistika';
+
+$lang["footer_main_h6_3"] = 'Individuálne úlohy';
+$lang["footer_individual_tasks_1_button_text"] = 'Szabóová Klaudia';
+$lang["footer_individual_tasks_2_button_text"] = 'Edi Gurmani';
+$lang["footer_individual_tasks_3_button_text"] = 'Dat NguyenThe';
+$lang["footer_individual_tasks_4_button_text"] = 'Blahó András';
+
+$lang["footer_copyright_text"] = 'Webové technológie 2 - FEI STU - 2020 letný semester';
+
+//*****************************************
+//INDEX.PHP
+//*****************************************
+
+$lang["index_main_h1_text"] = 'Hlavná stránka';
+$lang["index_main_h2_text"] = 'Lorem ipsum - SK';
+$lang["index_main_p_text"] = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nisi risus, aliquet a tortor ac, convallis eleifend lacus. Nam sed hendrerit velit. Sed in tortor cursus, posuere arcu vel, sodales velit. Nam elit dui, pharetra at massa sit amet, ultricies finibus nunc. Cras elementum vestibulum dapibus. Nullam pharetra erat non aliquam suscipit. Nulla semper mauris at dolor pulvinar, non vulputate arcu dictum. Aliquam dapibus gravida suscipit. Duis ipsum lectus, sodales sit amet feugiat sit amet, interdum quis leo. Nulla luctus velit at blandit congue. Donec tristique, elit a semper porttitor, purus erat tincidunt metus, tincidunt molestie ex nulla in purus.';
+
+//*****************************************
+//APIDESCRIPTION.PHP
+//*****************************************
+
+$lang["apidescription_main_h1_text"] = 'Popis API';
+
+//*****************************************
+//OCTAVECOMMANDS.PHP
+//*****************************************
+
+$lang["octavecommands_main_h1_text"] = 'Octave príkazy';
+$lang["octavecommands_input_text"] = 'Zadajte príkaz';
+$lang["octavecommands_btn_text"] = 'Vypočítaj!';
+
+//*****************************************
+//STATISTICS.PHP
+//*****************************************
+
+$lang["statistics_main_h1_text"] = 'Štatistika';
+$lang["statistics_table_header_1"] = 'Individuálna úloha';
+$lang["statistics_table_header_2"] = 'Používaná (kus)';
+$lang["statistics_button"] = 'Poslať štatistiku na email';
+
+//-----------------------------------------
+// !!!!! INDIVIDUAL TASKS START !!!!!
+//-----------------------------------------
+
+//Szabóová Klaudia
+$lang["szaboovaklaudia_main_h1_text"] = 'Szabóová Klaudia - tlmič kolesa ';
+$lang["szaboovaklaudia_input"] = 'Požadovaná výška skokovej prekážky';
+$lang["szaboovaklaudia_button"] = 'Vypočítať';
+$lang["szaboovaklaudia_check_1"] = 'Graf';
+$lang["szaboovaklaudia_check_2"] = 'Animácia';
+$lang["szaboovaklaudia_error"] = 'Hodnota musí byť medzi -0.3 a 0.3!';
+$lang["szaboovaklaudia_graf_label_1"] = 'Poloha kolesa';
+$lang["szaboovaklaudia_graf_label_2"] = 'Poloha vozidla';
+
+//Edi Gurmani
+$lang["edigurmani_main_h1_text"] = 'Edi Gurmani - Gulicka';
+$lang["edigurmani_check_1"] = 'Ukaz graf';
+$lang["edigurmani_check_2"] = 'Ukaz animaciu';
+$lang["edigurmani_error"] = 'Hodnota musi byt medzi 0 a 0.8!';
+$lang["edigurmani_button"] = 'Vypocitaj';
+$lang["edigurmani_graf_1_label"] = 'Náklon gulicku';
+$lang["edigurmani_graf_2_label"] = 'Naklon gulicku';
+
+
+//Dat NguyenThe
+$lang["datnguyenthe_main_h1_text"] = 'Dat NguyenThe - názov úlohy';
+$lang["datnguyenthe_example_1"] = 'Example 1';
+$lang["datnguyenthe_example_2"] = 'Example 2';
+
+//Blahó András
+$lang["blahoandras_main_h1_text"] = 'Blahó András - lietadlo';
+$lang["blahoandras_input_value_label"] = 'Nový náklon lietadla';
+$lang["blahoandras_button_label"] = 'Ukáž!';
+$lang["blahoandras_checkbox_graf_label"] = 'Sledovať graf';
+$lang["blahoandras_checkbox_animation_label"] = 'Sledovať animáciu';
+$lang["blahoandras_graf_1_label"] = 'Náklon lietadla';
+$lang["blahoandras_graf_2_label"] = 'Náklon zadnej klapky';
+?>
